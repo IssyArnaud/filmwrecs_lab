eb9e8afb1893bf9348b490cec52fce4702ba4a55
Issy Arnaud <yarnaud36@gmail.com>
2023-09-13 14:48:29 +0100

    tick box

--- controllers/film_controller.py
+++ controllers/film_controller.py
@@ -14,7 +14,29 @@ def index():
 @films_blueprint.route('/films')
 def show_all():
     films = Film.query.all()
-    return render_template('show_all.jinja', films=films)
+    return render_template('films/show_all.jinja', films=films)
 
-@films_blueprint.route('films/new')
+@films_blueprint.route("/films/watched/<id>", methods=["POST"])
+def watched(id):
+    film = Film.query.get(id)
+    film.watched = True
+    db.session.commit()
+    return redirect ('/films')
+
+@films_blueprint.route('/films/new')
+def new():
+    users = User.query.all()
+    return render_template('films/new.jinja', users=users)
+
+@films_blueprint.route('/films/new', methods=["POST"])
 def add_new():
+    title = request.form["title"]
+    genre = request.form["genre"]
+    release_date = request.form["release_date"]
+    run_time = request.form["run_time"]
+    user_id = request.form["user.id"]
+    new_film = Film(title=title, genre=genre, release_date=release_date, run_time=run_time, user_id=user_id)
+
+    db.session.add(new_film)
+    db.session.commit()
+    return redirect ('/films')
